62e53312a0f5440fd63b0bbf00cef677107e33eb
syaroslavtseva <svetlana-popova-1998@mail.ru>
2023-11-01 22:21:02 +0400

    Code refactoring

--- calculator.py
+++ calculator.py
@@ -1,5 +1,7 @@
 #Обработка аргументов командной строки
 import argparse
+import sys
+
 parser = argparse.ArgumentParser(description = 'Проект \"Калькулятор\" ')
 parser.add_argument('-o', '--operation', metavar = 'Operation', type = str, required = True, help = 'Выберите операцию (Введите addition (+), substraction (-), multiplication (*) или division (/) ). Указывается в формате: -o=x')
 parser.add_argument('-n1', '--number1', metavar = 'Number 1', type = float, required = True, help = 'Первое число. Указывается в формате: -n1=x')
@@ -21,22 +23,26 @@ def operation_division(number1, number2):
 
 #Проверка аргументов
 def operand_checking(number1, number2):
-    if args.number1<-1000 or args.number1>1000:
-        print('Введите значение n1 от -1000 до 1000')
-        return()
-    elif args.number2<-1000 or args.number2>1000:
-        print('Введите значение n2 от -1000 до 1000')
-        return()
+    if number1<-1000 or number1>1000:
+        return('Введите значение n1 от -1000 до 1000')
+    elif number2<-1000 or number2>1000:
+        return('Введите значение n2 от -1000 до 1000')
+    else:
+        return('')
 
+result = operand_checking(args.number1, args.number2)
+if result != '':
+    print(result)
+    sys.exit(0)
 
-operand_checking(args.number1, args.number2)
-
+operation_result = 0
 if args.operation.lower() == 'addition':
-    print(operation_addition(args.number1, args.number2))
+    operation_result = operation_addition(args.number1, args.number2)
 elif args.operation.lower() == 'substraction':
-    print(operation_substraction(args.number1, args.number2))
+    operation_result = operation_substraction(args.number1, args.number2)
 elif args.operation.lower() == 'multiplication':
-    print(operation_multiplication(args.number1, args.number2))
+    operation_result = operation_multiplication(args.number1, args.number2)
 elif args.operation.lower() == 'division':
-    print(operation_division(args.number1, args.number2))
-else: print('Неверная операция (Введите addition (+), substraction (-), multiplication (*) или division(/))')
+    operation_result = operation_division(args.number1, args.number2)
+else: operation_result = 'Неверная операция (Введите addition (+), substraction (-), multiplication (*) или division(/))'
+print(operation_result)
